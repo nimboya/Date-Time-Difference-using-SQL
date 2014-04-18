842831c7cca8e2439850bd78461adf56771ccb69
Ewere Diagboya <boya360@yahoo.com>
2014-04-18 10:21:24 +0100

    Create datediff.sql
    Check out this url: http://www.w3schools.com/sql/func_datediff.asp for more info on different return types of date difference

---
+++ datediff.sql
@@ -0,0 +1,6 @@
+// define two variables for Date1 and Date2
+// Using the DATEDIFF function which is a function in SQL
+// Ensure both values are date format in your Database of your choice
+// The function returns 
+SELECT DATEDIFF(Date1, Date2) FROM posts
+
